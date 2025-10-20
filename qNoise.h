295b5b1d8cbe68eb815b3da22c9f0e2699c74c07
Ignacio Deza <ignaciodeza@users.noreply.github.com>
2025-10-20 10:58:22 +0100

    Revise copyright and documentation in qNoise.h
    Updated copyright information, added permission notice, and improved description and citation for qNoise.

--- qNoise.h
+++ qNoise.h
@@ -1,41 +1,49 @@
 /*
 qNoise: A generator of non-Gaussian colored noise
-Copyright © 2021, Juan Ignacio Deza
-email: ignacio.deza@uwe.ac.uk
+Copyright (c) 2021-2025 J. Ignacio Deza
 
-Description
-qNoise is a non-gaussian colored random noise generator. It is a handy source of
-self-correlated noise for a great variety of applications. It depends on two
-parameters only: tau for controlling the autocorrelation, and q for controlling
-the statistics. This noise tends smoothly  for q = 1 to an  Ornstein-Uhlenbeck
-(colored gaussian) noise with autocorrelation tau. for q < 1 it is bounded noise
-and it is supra-Gaussian for q > 1. The noise is generated  via a stochastic
-differential equation using the Heun method (a second order Runge-Kutta type
-integration scheme) and it is implemented as a stand-alone library in c++. It
-Useful as input for numerical simulations, as a source of noise for controlling
-experiments using synthetic noise via micro-controllers and for a wide variety
-of applications.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-Requirements
-It is a stand-alone library with no dependencies other than the standard
-libraries. Due to it's use of some functions from the <random> library the
-library currently works on c++11 or higher only. This should be OK for most Macs
-and new Linux systems. In some older systems it is possible that you need to add
-`-std=gnu++11` to your compilation flags.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-Licence
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+---
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Description:
+qNoise is a non-Gaussian colored random noise generator. It is a handy source 
+of self-correlated noise for a great variety of applications. It depends on 
+two parameters only: tau for controlling the autocorrelation, and q for 
+controlling the statistics.
+
+- q = 1: Ornstein-Uhlenbeck (colored Gaussian) noise
+- q < 1: Bounded noise (sub-Gaussian)
+- q > 1: Heavy-tailed noise (supra-Gaussian)
+
+The noise is generated via a stochastic differential equation using the Heun 
+method (a second-order Runge-Kutta integration scheme).
+
+Requirements:
+- C++11 or higher (uses <random> library)
+- No external dependencies
+
+Contact: ignacio.deza@uwe.ac.uk
+
+Citation:
+Deza, J.I., Ihshaish, H. (2022). qNoise: A generator of non-Gaussian colored 
+noise. SoftwareX, 18, 101034. https://doi.org/10.1016/j.softx.2022.101034
 */
 
 #ifndef QNOISEGEN_H
